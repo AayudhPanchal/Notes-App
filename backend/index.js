86e35954a7c4e3b78242c8faa5d1dbe8bb3943e7
Aayudh P Panchal <aayudhpanchal@gmail.com>
2024-10-01 19:15:59 +0530

    Added Signup and Login APIs, along with User and Notes Models

---
+++ backend/index.js
@@ -0,0 +1,112 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const jwt = require("jsonwebtoken")
+const { authenticateToken } = require('./utils')
+const User = require('./models/user.model')
+
+mongoose.connect(process.env.CONNECTION_STRING)
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({data: "hello"});
+})
+
+app.post("/create-account", async (req, res) => {
+    
+    const {fullName, email, password} = req.body
+
+    if(!fullName){
+        return res.status(400).json({error: true, message: "Please Enter your Name."})
+    }
+    if(!email){
+        return res.status(400).json({error: true, message: "Please Enter your Email."})
+    }
+    if(!password){
+        return res.status(400).json({error: true, message: "Please Enter your Password."})
+    }
+
+    const isUser = await User.findOne({email: email})
+
+    if(isUser){
+        return res.json({
+            error: true,
+            message: "User already exists"
+        })
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password
+    })
+
+    await user.save()
+
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN, {
+        expiresIn: "36000m"
+    });
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Successful!"
+    })
+})
+
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email){
+        return res.status(400).json({error: true, message: "Please Enter your Email"});
+    }
+    if(!password){
+        return res.status(400).json({error: true, message: "Please Enter your Password"});
+    }
+
+    const userInfo = await User.findOne({email: email});
+
+    if(!userInfo){
+        return res.status(400).json({error: true, message: "User not found"})
+    }
+
+    if(userInfo.email == email && userInfo.password == password){
+        const user = {user: userInfo};
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+            expiresIn: "36000m"
+        });
+
+        return res.json({
+            error: false,
+            message: "Login Successful!",
+            email,
+            accessToken
+        })
+    }
+    else{
+        return res.json({
+            error: true,
+            message: "Incorrect Password"
+        })
+    }
+
+})
+
+app.post("/add-note", authenticateToken, async (req, res) => {
+    
+})
+
+app.listen(8000);
+
+module.exports = app;
